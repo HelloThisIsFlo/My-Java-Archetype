0a8dd790dffd32c9fdb1fdbe56f6d2a8f47955e4
Florian Kempenich <shockn745@gmail.com>
2017-01-14 14:04:11 +0000

    Initial commit

---
+++ src/main/java/com/professionalbeginner/MyarchetypeApplication.java
@@ -0,0 +1,12 @@
+package com.professionalbeginner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyarchetypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyarchetypeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/professionalbeginner/MyarchetypeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.professionalbeginner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyarchetypeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
