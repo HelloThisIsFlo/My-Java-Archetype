507db990f3d6d7a9d352b0c0182433e1fb5eab04
Florian Kempenich <shockn745@gmail.com>
2017-01-15 13:45:11 +0000

    Finish implementation in-memory book repository

--- src/main/java/com/professionalbeginner/data/inmemory/InMemoryBookRepository.java
+++ src/main/java/com/professionalbeginner/data/inmemory/InMemoryBookRepository.java
@@ -2,10 +2,13 @@ package com.professionalbeginner.data.inmemory;
 
 import com.professionalbeginner.domain.core.book.Book;
 import com.professionalbeginner.domain.core.book.BookId;
+import com.professionalbeginner.domain.interfacelayer.repository.BookNotFoundException;
 import com.professionalbeginner.domain.interfacelayer.repository.BookRepository;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author Kempenich Florian
@@ -20,23 +23,60 @@ public class InMemoryBookRepository implements BookRepository {
 
     @Override
     public BookId save(Book book) {
+        if (hasBook(book.id())) {
+            return updateExisting(book);
+        } else {
+            return saveNew(book);
+        }
+    }
+
+    private boolean hasBook(BookId id) {
+        return id.idLong() >= 1 && id.idLong() <= books.size();
+    }
+
+    private BookId updateExisting(Book book) {
+        BookId id = book.id();
+        int index = getIndex(id);
+
+        books.remove(index);
+        books.add(book);
+
+        return id;
+    }
+
+    private int getIndex(BookId id) {
+        return (int) (id.idLong() - 1);
+    }
+
+    private BookId saveNew(Book book) {
         long id = books.size() + 1;
         books.add(book);
         return new BookId(id);
     }
 
     @Override
-    public Book findById(BookId id, boolean withReviews) {
-        Book book =  books.get(((int) id.idLong()) - 1);
-        if(!withReviews) {
-            book = copyWithoutReviews(book);
+    public Book findById(BookId id, boolean withReviews) throws BookNotFoundException {
+        try {
+            Book book = books.get(getIndex(id));
+            if(!withReviews) {
+                book = copyWithoutReviews(book);
+            }
+            return book;
+        } catch (Exception e) {
+            throw new BookNotFoundException(id);
         }
-        return book;
     }
 
     @Override
     public List<Book> findAll(boolean withReviews) {
-        throw new IllegalStateException("Not implemented");
+        List<Book> result = new ArrayList<>(books);
+        if (withReviews) {
+            return result;
+        } else {
+            return result.stream()
+                    .map(this::copyWithoutReviews)
+                    .collect(Collectors.toList());
+        }
     }
 
     private Book copyWithoutReviews(Book book) {
--- src/test/java/com/professionalbeginner/data/inmemory/InMemoryBookRepositoryTest.java
+++ src/test/java/com/professionalbeginner/data/inmemory/InMemoryBookRepositoryTest.java
@@ -6,11 +6,15 @@ import com.professionalbeginner.domain.core.book.BookId;
 import com.professionalbeginner.domain.core.book.Price;
 import com.professionalbeginner.domain.core.review.Review;
 import com.professionalbeginner.domain.core.review.ReviewId;
+import com.professionalbeginner.domain.interfacelayer.repository.BookNotFoundException;
 import com.professionalbeginner.domain.interfacelayer.repository.BookRepository;
 import com.professionalbeginner.domain.interfacelayer.repository.ReviewRepository;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.springframework.beans.factory.access.BootstrapException;
 
+import javax.transaction.Transactional;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
@@ -23,23 +27,31 @@ import static org.junit.Assert.*;
 public class InMemoryBookRepositoryTest {
 
     private BookRepository bookRepository;
-    private ReviewRepository reviewRepository;
     private TestUtils testUtils;
     private Random random;
 
     @Before
     public void setUp() throws Exception {
+        random = new Random();
         bookRepository = new InMemoryBookRepository();
-        reviewRepository = null;
         testUtils = new TestUtils();
+    }
+
+    @Test(expected = BookNotFoundException.class)
+    public void bookNotFound_throwException() throws Exception{
+        bookRepository.findById(new BookId(333L), false);
     }
 
     @Test
     public void saveAndRetrieve() throws Exception {
+        // Create and persist book
         Book book = testUtils.makeRandomBook(BookId.NOT_ASSIGNED);
-
         BookId savedId = bookRepository.save(book);
         book.setId(savedId);
+
+        // Add reviews
+        createRandomReviewWithRadomId(savedId).forEach(book::addReview);
+        bookRepository.save(book);
 
         Book fromRepoWithReviews = bookRepository.findById(savedId, true);
         assertTrue("Books are not similar", areBooksSimilar(book, fromRepoWithReviews));
@@ -52,17 +64,65 @@ public class InMemoryBookRepositoryTest {
         assertTrue("Review list shoudl be empty", fromRepoWithoutReviews.getReviews().isEmpty());
     }
 
+    @Test
+    public void saveWithExistingId_updates() throws Exception {
+        Book book = testUtils.makeRandomBook(BookId.NOT_ASSIGNED);
+        BookId id = bookRepository.save(book);
+
+        Book updatedBook = testUtils.makeRandomBook(id);
+        BookId updatedId = bookRepository.save(updatedBook);
+
+        assertEquals(id, updatedId);
+
+        Book fromRepo = bookRepository.findById(updatedId, true);
+        assertEquals(updatedBook, fromRepo);
+    }
+
+    @Test
+    public void saveAll_findAll() throws Exception {
+        // Create and persist books
+        Book book1 = testUtils.makeRandomBook(BookId.NOT_ASSIGNED);
+        Book book2 = testUtils.makeRandomBook(BookId.NOT_ASSIGNED);
+        Book book3 = testUtils.makeRandomBook(BookId.NOT_ASSIGNED);
+        BookId savedId1 = bookRepository.save(book1);
+        BookId savedId2 = bookRepository.save(book2);
+        BookId savedId3 = bookRepository.save(book3);
+        book1.setId(savedId1);
+        book2.setId(savedId2);
+        book3.setId(savedId3);
+        List<Book> books = Arrays.asList(book1, book2, book3);
+
+        // Add reviews to first book
+        createRandomReviewWithRadomId(savedId1).forEach(book1::addReview);
+        bookRepository.save(book1);
+
+
+        List<Book> withReviews = bookRepository.findAll(true);
+        List<Book> withoutReviews = bookRepository.findAll(false);
+
+        assertEqualsIgnoreOrder(books, withReviews);
+        books.forEach(
+                book -> assertListContainsSimilarBook_ignoreReview(book, withoutReviews)
+        );
+        withoutReviews.forEach(this::assertReviewListEmtpy);
+    }
+
+    private void assertReviewListEmtpy(Book book) {
+        assertTrue("Review list should be empty", book.getReviews().isEmpty());
+    }
+
+    private <T> void assertEqualsIgnoreOrder(List<T> first, List<T> second) {
+        String errorMessage = "List not equivalent: \n" +
+                first + "\n" +
+                second + "\n";
+        assertTrue(errorMessage, first.containsAll(second));
+        assertTrue(errorMessage, second.containsAll(first));
+    }
+
     private List<Review> createRandomReviewWithRadomId(BookId bookId) {
         Review review1 = testUtils.makeRandomReview(randomId(), bookId);
         Review review2 = testUtils.makeRandomReview(randomId(), bookId);
         Review review3 = testUtils.makeRandomReview(randomId(), bookId);
-        ReviewId id1 = reviewRepository.save(review1);
-        ReviewId id2 = reviewRepository.save(review2);
-        ReviewId id3 = reviewRepository.save(review3);
-        review1.setId(id1);
-        review2.setId(id2);
-        review3.setId(id3);
-
         return Arrays.asList(review1, review2, review3);
     }
 
